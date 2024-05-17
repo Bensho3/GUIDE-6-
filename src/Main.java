eba1db6b57cefd66442341206773911e160c6f84
Benja <benchudelavega@gmail.com>
2024-05-17 20:01:44 -0300

    Primer paso

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Map <Character, Integer> mapChain = new HashMap();
+
+        Scanner s = new Scanner(System.in);
+        String screen = s.nextLine();
+
+        for(int i = 0; i < screen.length(); i++ ) {
+        System.out.println(screen.charAt(i));
+        }
+
 
         }
     }
